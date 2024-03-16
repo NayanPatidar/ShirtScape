3fbcaf153d253fea3432bb4600c8e2b3e5bc3250
Nayan Patidar <nayanpatidar28@gmail.com>
2024-03-16 23:08:01 +0530

    Added baner

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Nav";
+import banner from "../../assets/banner.jpg"
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <div><img src={banner} /></div>
     </div>
   );
 }
