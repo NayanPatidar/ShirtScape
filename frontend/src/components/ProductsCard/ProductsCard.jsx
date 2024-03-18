7618558ef18799fe54bb44b5bcf7ece5a9ad5e59
Nayan Patidar <nayanpatidar28@gmail.com>
2024-03-18 14:49:55 +0530

    ProductsCardChanges

--- frontend/src/components/ProductsCard/ProductsCard.jsx
+++ frontend/src/components/ProductsCard/ProductsCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ProductsCard.css"
 
 const Cards = () => {
   return (
@@ -7,10 +8,10 @@ const Cards = () => {
         className=" rounded-3xl"
         src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13622286/2024/2/19/a0794667-e541-4526-958d-6c9536a0009b1708340067741RoadsterMenGreenWhiteColourblockedRoundNeckPureCottonT-shirt1.jpg"
       />
-      <div className=" text-md flex flex-col">
-        <span className="ProductName pl-3 pr-3 ">Naruto: Jiyara</span>
-        <span className="ProductDesc pl-3 pr-3 ">dsfs</span>
-        <span className="ProductPrice pl-3 pr-3 ">$599</span>
+      <div className=" flex flex-col">
+        <span className="ProductName">Naruto: Jiyara</span>
+        <span className="ProductDesc">Oversized T-Shirt</span>
+        <span className="ProductPrice">$ 599</span>
       </div>
     </div>
   );
