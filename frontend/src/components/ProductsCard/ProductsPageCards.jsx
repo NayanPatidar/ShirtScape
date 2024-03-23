caecc389691662ce5292574bd642d29ded9c51fe
Nayan Patidar <nayanpatidar28@gmail.com>
2024-03-24 00:09:43 +0530

    ProductsPageCard Created

---
+++ frontend/src/components/ProductsCard/ProductsPageCards.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ProductsPageCards.css";
+
+const ProductsPageCard = ({
+  Image,
+  Name,
+  Description,
+  Price,
+  MRP,
+  Discount,
+}) => {
+  return (
+    <div className=" ProductsPageCardsMain flex flex-col ">
+      <img className=" ProductsPageImage cursor-pointer" src={Image} />
+      <div className=" flex flex-col">
+        <span className="ProductsPageName">{Name}</span>
+        <span className="ProductsPageDesc">{Description}</span>
+        <div className=" flex flex-row justify-start gap-2">
+          <span className="ProductsPagePrice">Rs. {Price}</span>
+          <span className="ProductsPageMRP">Rs. {MRP}</span>
+          <span className="ProductsPageDiscount">{Discount}% OFF</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsPageCard;
