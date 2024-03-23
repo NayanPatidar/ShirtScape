61cb57cbc93cc76c53ce1ff9e50d9a8c86b38c5f
Nayan Patidar <nayanpatidar28@gmail.com>
2024-03-23 19:27:00 +0530

    ProductsPageDesigned

--- frontend/src/pages/Products/ProductsPage.jsx
+++ frontend/src/pages/Products/ProductsPage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "./ProductsPage.css";
+import banner4 from "../../assets/banner4.png";
+
+const divStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "400px",
+  backgroundSize: "cover",
+};
 
 const ProductsPage = () => {
-  return <div></div>;
+  return (
+    <div className="ProductsPageMain flex flex-col pt-16">
+      {/* <div style={{ ...divStyle }}>
+        <img src={banner4} />
+      </div> */}
+      <div className=" ProductDescriptionArea"></div>
+      <div className=" flex flex-row">
+        <div className="FilterTableMain"></div>
+        <div className="ProductsListMain"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsPage;
