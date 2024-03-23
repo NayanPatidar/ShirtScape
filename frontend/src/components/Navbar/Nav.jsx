6c6c744e291c564496c781947a0001400651ff10
Nayan Patidar <nayanpatidar28@gmail.com>
2024-03-23 19:26:44 +0530

    Changed Nav Font

--- frontend/src/components/Navbar/Nav.jsx
+++ frontend/src/components/Navbar/Nav.jsx
@@ -9,14 +9,14 @@ import { useNavigate } from 'react-router-dom'
 function Navbar() {
   let navigate = useNavigate();
   return (
-    <div className="NavBarMain flex flex-col fixed">
+    <div className="NavBarMain flex flex-col fixed shadow-md">
       <div className=" h-16 bg-white pl-5 flex flex-row items-center justify-between">
         <div className="flex flex-row justify-start items-center w-3/5">
-          <img src={logo} alt="LOGO" className="logo w-20" onClick={() => navigate("/")}/>
+          <img src={logo} alt="LOGO" className="logo w-16" onClick={() => navigate("/")}/>
           <div className="NavBarObj flex flex-row justify-around w-8/12 text-black">
-            <div className=" hover:underline text-black cursor-pointer" onClick={() => navigate("/products")}>MEN</div>
-            <div className=" hover:underline text-black cursor-pointer">NEW ARRIVALS</div>
-            <div className=" text-black text-md">
+            <div className=" hover:underline text-black cursor-pointer mt-2" onClick={() => navigate("/products")}>MEN</div>
+            <div className=" hover:underline text-black cursor-pointer mt-2">NEW ARRIVALS</div>
+            <div className=" text-black text-md pb-4">
               <form className="SearchBox">
                 <label>
                   <input
@@ -35,7 +35,6 @@ function Navbar() {
           <CiShoppingCart className=" size-8 text-black" />
         </div>
       </div>
-      <div className=" h-1 bg-black"></div>
     </div>
   );
 }
