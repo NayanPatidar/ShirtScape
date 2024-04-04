858f90f9deb7c4adf4a353c76b4be96867620dd4
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-04 20:35:06 +0530

    Added Product presence Check

--- frontend/src/services/AddProduct.js
+++ frontend/src/services/AddProduct.js
@@ -7,13 +7,17 @@ function AddProduct({ product_id }) {
   if (localStorage.getItem("ShirtScape_Cart")) {
     let cart = JSON.parse(localStorage.getItem("ShirtScape_Cart"));
 
- 
+    if (!cart.some((item) => item.id === product_id)) {
+      cart.push(Cart_Product);
+      localStorage.setItem("ShirtScape_Cart", JSON.stringify(cart));
+      return "Product Added to Cart";
+    }
 
-    cart.push(Cart_Product);
-    localStorage.setItem("ShirtScape_Cart", JSON.stringify(cart));
+    return "Product Already Present";
   } else {
     let cart = [Cart_Product];
     localStorage.setItem("ShirtScape_Cart", JSON.stringify(cart));
+    return "Product Added to Cart";
   }
 }
 
