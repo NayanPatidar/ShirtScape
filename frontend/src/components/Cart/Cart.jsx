9df5d9096b7779e4a9cfed9d416fc79fb36ad309
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-04 15:33:31 +0530

    Added the Card Basic Code

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div className=" w-screen flex flex-col">
+        <div className=" bg-gray-800 h-16"></div>
+        <div className=" flex flex-row justify-center gap-4 p-5">
+          <div className="  w-3/6">
+            <div className="ProductsBlockMain text-black"></div>
+          </div>
+          <div className="ProductsPricesBlock w-2/6 text-black"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
