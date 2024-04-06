641ef67d50dc60189d6619822ba3ff4e0e731110
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-06 12:19:31 +0530

    CartProduct Changes

--- frontend/src/components/CartProdSize&Quantity/CartProductSize.jsx
+++ frontend/src/components/CartProdSize&Quantity/CartProductSize.jsx
@@ -5,12 +5,50 @@ import { CartDropdownContext } from "../../contexts/CartDropDown";
 import "./CartProduct.css";
 
 const CartProductSize = ({ products, index }) => {
-  console.log("Here weee aaree");
+  const [selectedSize, setSelectedSize] = useState(null);
+
+  const handleSizeClick = (size) => {
+    setSelectedSize(size);
+  };
+
   return (
-    <div className="modal-overlay ">
-      <div className="modal">
-        <h2>Select Ssdfsfsdfize</h2>
-        <button>Close</button>
+    <div>
+      <div className="SizeSelectionBox flex flex-col justify-center align-middle items-center">
+        <div className=" ClothData flex flex-col">
+          <div className="ImgageCloth">
+            {products && <img src={products.cloths.photo1} />}
+          </div>
+          <div className=""></div>
+        </div>
+        <div className=" flex gap-10 mt-5 cursor-pointer">
+          <span
+            className={`sizeDot ${selectedSize === "S" ? "active" : ""}`}
+            onClick={() => handleSizeClick("S")}
+          >
+            S
+          </span>
+          <span
+            className={`sizeDot ${selectedSize === "M" ? "active" : ""}`}
+            onClick={() => handleSizeClick("M")}
+          >
+            M
+          </span>
+          <span
+            className={`sizeDot ${selectedSize === "L" ? "active" : ""}`}
+            onClick={() => handleSizeClick("L")}
+          >
+            L
+          </span>
+          <span
+            className={`sizeDot ${selectedSize === "XL" ? "active" : ""}`}
+            onClick={() => handleSizeClick("XL")}
+          >
+            XL
+          </span>
+        </div>
+        <div className=" submitSize flex justify-center align-middle items-center">
+          <button></button>
+        </div>
       </div>
     </div>
   );
