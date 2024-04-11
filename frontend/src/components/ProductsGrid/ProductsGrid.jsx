faaa151bd4e8c9b1bf9e6d29b263dc1e647574b4
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-11 16:15:08 +0530

    No Cloths Text

--- frontend/src/components/ProductsGrid/ProductsGrid.jsx
+++ frontend/src/components/ProductsGrid/ProductsGrid.jsx
@@ -4,22 +4,28 @@ import ProductsPageCard from "../ProductsCard/ProductsPageCards";
 const ProductGrid = ({ productsData }) => {
   return (
     <div className="ProductsListMain p-5 pb-24 ">
-      <div className=" ProductsArea grid grid-cols-4 gap-y-24 justify-items-center">
-        {productsData &&
-          productsData.map((product, index) => (
-            <div key={index} className=" ProductBoxMain z-0 ">
-              <ProductsPageCard
-                Image={product.cloths.photo1}
-                Name={product.cloths.product_name}
-                Description={product.cloths.genericdesc}
-                Price={product.cloths.price}
-                MRP={product.cloths.mrp}
-                Discount={product.cloths.discount}
-                ProductId={product.cloths.product_id}
-              />
-            </div>
-          ))}
-      </div>
+      {productsData &&
+        (productsData.length > 0 ? (
+          <div className=" ProductsArea grid grid-cols-4 gap-y-24 justify-items-center">
+            {productsData.map((product, index) => (
+              <div key={index} className=" ProductBoxMain z-0 ">
+                <ProductsPageCard
+                  Image={product.cloths.photo1}
+                  Name={product.cloths.product_name}
+                  Description={product.cloths.genericdesc}
+                  Price={product.cloths.price}
+                  MRP={product.cloths.mrp}
+                  Discount={product.cloths.discount}
+                  ProductId={product.cloths.product_id}
+                />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <span className=" flex w-full text-3xl font-semibold justify-center items-center">
+            No Cloths Available
+          </span>
+        ))}
     </div>
   );
 };
