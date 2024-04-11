ded1eefe3ffff885f0be697eb7a6c85544729f65
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-11 08:47:05 +0530

    Added the Confimation Box

---
+++ frontend/src/components/ConfirmRemoval/ConfirmationBox.jsx
@@ -0,0 +1,62 @@
+import React, { useContext, useEffect, useState } from "react";
+import { CartContext } from "../../contexts/CartSizeSelection";
+import { IoMdClose } from "react-icons/io";
+import "./Confirmation.css";
+import { RiErrorWarningFill } from "react-icons/ri";
+import { DeleteProductFromLocalStorage } from "../../services/storageOperations";
+
+const ItemRemovalBox = () => {
+  const {
+    IsItemRemovalMenuOpen,
+    setIsItemRemovalMenuOpen,
+    productId,
+    ItemRemovalDone,
+    setRemovalDone,
+  } = useContext(CartContext);
+
+  const handleYes = () => {
+    DeleteProductFromLocalStorage(productId);
+    setRemovalDone(!ItemRemovalDone);
+    handleCloseRemovalBox();
+  };
+
+  const handleNo = () => {
+    handleCloseRemovalBox();
+  };
+
+  const handleCloseRemovalBox = () => {
+    if (IsItemRemovalMenuOpen) {
+      setIsItemRemovalMenuOpen(false);
+    }
+  };
+
+  return (
+    <div className="ItemRemovalBox flex flex-col justify-between ">
+      <div className=" flex flex-row w-full justify-between items-center">
+        <span className=" RemovalTitle flex items-center gap-2">
+          <RiErrorWarningFill />
+          Remove the Item from the Cart
+        </span>
+        <span>
+          <IoMdClose
+            onClick={handleCloseRemovalBox}
+            className="cursor-pointer"
+          />
+        </span>
+      </div>
+      <span className=" RemovalDesc">
+        Are you sure you want to remove the item from the cart ?
+      </span>
+      <div className=" submitSize flex justify-center align-middle items-center gap-10">
+        <button className=" SubmitRemoval " onClick={() => handleYes()}>
+          YES
+        </button>
+        <button className=" SubmitRemoval " onClick={() => handleNo()}>
+          NO
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ItemRemovalBox;
