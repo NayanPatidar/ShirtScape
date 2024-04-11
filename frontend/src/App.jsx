305f998f449ac8f62b46200c00f998f47056edb1
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-11 16:14:51 +0530

    Modified the Search Context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./pages/Home/Home";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
@@ -6,19 +6,24 @@ import ProductsPage from "./pages/Products/ProductsPage";
 import Wishlist from "./components/Wishlist/Wishlist";
 import Product from "./components/Product/Product";
 import Cart from "./pages/Cart/Cart";
+import { SearchContext } from "./contexts/contexts";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/men" element={<ProductsPage />} />
-        <Route path="/products">
-          <Route path=":productId" element={<Product />} />
-        </Route>
-        <Route path="/checkout/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-      </Routes>
+      <SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/men" element={<ProductsPage />} />
+          <Route path="/products">
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/checkout/cart" element={<Cart />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+        </Routes>
+      </SearchContext.Provider>
     </div>
   );
 }
