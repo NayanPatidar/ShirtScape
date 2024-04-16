726c9b12e80b173ddc92aa7c9c37740ecdcc695e
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-17 01:06:03 +0530

    Profile Meno Options

---
+++ frontend/src/pages/Profile/Address.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Nav";
+import "./Menu.css";
+import ProfileMenu from "../../components/ProfileMenu/ProfileMenu";
+
+const Address = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className=" flex flex-row gap-4 pt-24 justify-center">
+        <div className="w-1/6">
+          <ProfileMenu />
+        </div>
+        <div className=" h-96 w-4/6"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Address;
---
+++ frontend/src/pages/Profile/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Nav";
+import "./Menu.css";
+import ProfileMenu from "../../components/ProfileMenu/ProfileMenu";
+
+const Profile = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className=" flex flex-row gap-4 pt-24 justify-center">
+        <div className="w-1/6">
+          <ProfileMenu />
+        </div>
+        <div className="MyProfile h-96 w-4/6 pl-10">
+          <span className=" MyProfileTitle">MY PROFILE</span>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
