a6eb97f48aa5a0424fc5ab189597be4b610fc7e8
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-17 17:38:46 +0530

    AuthProvider

--- frontend/src/contexts/contexts.jsx
+++ frontend/src/contexts/contexts.jsx
@@ -1,4 +1,36 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 export const SearchContext = createContext();
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(
+    localStorage.getItem("isUserLoggedIn") === "true"
+  );
+
+  useEffect(() => {
+    const storedIsUserLoggedIn = localStorage.getItem("isUserLoggedIn");
+    if (storedIsUserLoggedIn !== null) {
+      setIsUserLoggedIn(storedIsUserLoggedIn === "true");
+    }
+  }, []);
+
+  const login = () => {
+    setIsUserLoggedIn(true);
+    localStorage.setItem("isUserLoggedIn", "true");
+  };
+
+  const logout = () => {
+    setIsUserLoggedIn(false);
+    localStorage.setItem("isUserLoggedIn", "false");
+  };
+
+  return (
+    <AuthContext.Provider value={{ isUserLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthContextProvider };
