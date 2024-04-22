735a8aedef5507b42164b9dbdaa0ef250638e1ba
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-22 23:49:33 +0530

    Delete Handler

--- frontend/src/handlers/AddressHandlers.js
+++ frontend/src/handlers/AddressHandlers.js
@@ -12,7 +12,7 @@ export const AddAddress = async (address) => {
       body: JSON.stringify(address),
     };
 
-    fetch("/address/add", options)
+    fetch("http://localhost:8080/address/add", options)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -25,9 +25,39 @@ export const AddAddress = async (address) => {
       .catch((error) => {
         console.error("Error adding address:", error);
       });
-  } catch (error) {}
+  } catch (error) {
+    console.error("Failed to Add the Address : ", error.message);
+  }
 };
 
 export const UpdateAddress = async (address) => {};
 
-export const DeleteAddress = async (address) => {};
+export const DeleteAddress = async (addressID) => {
+  try {
+    const token = getCookie("sscape");
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ AddressId: addressID }),
+    };
+
+    fetch("http://localhost:8080/address/delete", options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Address deletion successful:", data.message);
+      })
+      .catch((error) => {
+        console.error("Error adding address:", error);
+      });
+  } catch (error) {
+    console.error("Failed to Add the Address : ", error.message);
+  }
+};
