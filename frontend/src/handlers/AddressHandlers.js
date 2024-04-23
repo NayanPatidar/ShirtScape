b3faed2f97c148d31b1d8993b478e87cde624235
Nayan Patidar <nayanpatidar28@gmail.com>
2024-04-23 11:19:32 +0530

    Update Address Handler

--- frontend/src/handlers/AddressHandlers.js
+++ frontend/src/handlers/AddressHandlers.js
@@ -30,7 +30,38 @@ export const AddAddress = async (address) => {
   }
 };
 
-export const UpdateAddress = async (address) => {};
+export const UpdateAddress = async (addressID, UpdatedAddress) => {
+  console.log(
+    JSON.stringify({ AddressId: addressID, Address: UpdatedAddress })
+  );
+  try {
+    const token = getCookie("sscape");
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ AddressId: addressID, Address: UpdatedAddress }),
+    };
+
+    fetch("http://localhost:8080/address/update", options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Address Modified successful:", data.message);
+      })
+      .catch((error) => {
+        console.error("Error adding address:", error);
+      });
+  } catch (error) {
+    console.error("Failed to Add the Address : ", error.message);
+  }
+};
 
 export const DeleteAddress = async (addressID) => {
   try {
